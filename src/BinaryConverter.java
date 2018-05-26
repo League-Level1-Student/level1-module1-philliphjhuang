d6ee7d2b2c73dc5fed4119d03905c9d06926c754
jbear18 <league@WTS5.attlocal.net>
2018-05-25 18:08:37 -0700

    binary converter
    .

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -3,11 +3,14 @@ import java.awt.event.MouseListener;
 import java.lang.reflect.Executable;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class BinaryConverter implements MouseListener {
+	JTextField textField = new JTextField(20);
+	JLabel label = new JLabel();
 	
 	
 	
@@ -15,7 +18,7 @@ public class BinaryConverter implements MouseListener {
 public static void main(String[] args) {
 	new BinaryConverter().binary();
 	
-	
+
 	
 	
 	
@@ -24,38 +27,48 @@ public static void main(String[] args) {
 void binary() {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JTextField textField = new JTextField(20);
+	
 	JButton button = new JButton();
+	
+	
 	
 	frame.setVisible(true);
 	frame.setSize(300, 300);
 	frame.add(panel);
 	panel.add(textField);
+	panel.add(button);
+	button.setText("Convert");
+	panel.add(label);
+	label.setText("A");
 	frame.pack();
 	button.addMouseListener(this);
 	
-}
-@Override
+} 
+
 public void mouseClicked(MouseEvent e) {
+	
 	// TODO Auto-generated method stub
-	 String convert(String input) {
-         if(input.length() != 8){
-              JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
-              return "-";
-         }
-         String binary = "[0-1]+";    //must contain numbers in the given range
-         if (!input.matches(binary)) {
-              JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
-              return "-";
-         }
-         try {
-              int asciiValue = Integer.parseInt(input, 2);
-              char theLetter = (char) asciiValue;
-              return "" + theLetter;
-         } catch (Exception e) {
-              JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
-              return "-";
-         }
+	label.setText(convert(textField.getText()));
+	 System.out.print(convert(textField.getText()));
+	 
+}
+String convert(String input) {
+    if(input.length() != 8){
+         JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+         return "-";
+    }
+    String binary = "[0-1]+";    //must contain numbers in the given range
+    if (!input.matches(binary)) {
+         JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+         return "-";
+    }
+    try {
+         int asciiValue = Integer.parseInt(input, 2);
+         char theLetter = (char) asciiValue;
+         return "" + theLetter;
+    } catch (Exception e) {
+         JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+         return "-";
     }
 }
 @Override
